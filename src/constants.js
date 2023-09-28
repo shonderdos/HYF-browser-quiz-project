92af630a586d8249244db359201fe49873254aee
Saleh Habtor <saleh.tiger2@gmail.com>
2023-09-28 14:46:52 +0200

    added play-again functionality

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,4 @@ export const START_QUIZ_BUTTON_ID = 'start-quiz-button';
 export const ANSWERS_LIST_ID = 'answers-list';
 export const NEXT_QUESTION_BUTTON_ID = 'next-question-button';
 export const WHISKERS_IMG = 'whiskers-image';
+export const PLAY_AGAIN_BUTTON_ID = 'play-again-button';
